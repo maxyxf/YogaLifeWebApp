b628f9cea460d37c4367c3f25510ee315af68656
Max <mikowind@outlook.com>
2023-04-07 14:38:51 -0700

    add product detail page

--- client/src/screens/ProductDetail.js
+++ client/src/screens/ProductDetail.js
@@ -1,5 +1,46 @@
 import React from "react";
+import products from "../products";
+import { useParams } from "react-router-dom";
 
 export default function ProductDetail() {
-  return <p>ProductDetail</p>;
+  const { id } = useParams();
+  console.log("products:", products);
+  const product = products.find((p) => p.id === parseInt(id));
+  console.log(product);
+
+  return (
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div className="flex justify-between items-center px-6 py-4 bg-gray-100">
+          <div className="text-gray-600">{product.name}</div>
+        </div>
+
+        <div className="flex flex-wrap justify-between px-6 py-4">
+          <div className="w-full md:w-6/12 lg:w-5/12">
+            <img
+              src={product.imageSrc}
+              alt={product.name}
+              className="w-full object-contain"
+            />
+          </div>
+
+          <div className="flex justify-center items-center w-full md:w-6/12 lg:w-7/12 px-4 py-8 md:py-0">
+            <div className="flex flex-col justify-center items-center h-full w-full">
+              <div className="flex items-center">
+                <span className="text-gray-600">{product.price}</span>
+              </div>
+
+              <button
+                className={
+                  "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-30"
+                }
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
