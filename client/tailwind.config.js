10965a1814c02bead764ac260abc08d44ec47e0e
Max <mikowind@outlook.com>
2023-04-07 00:28:49 -0700

    create header

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      lightBrown: "#c2b8a8",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
