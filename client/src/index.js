ec6abf021ad7d74c5073259c7b990888fb4244bb
Max <mikowind@outlook.com>
2023-04-12 13:53:49 -0700

    create AuthTokenContext file, add Auth0Provider and AuthTokenProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+import { AuthTokenProvider } from "./AuthTokenContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const requestedScopes = [
+  "profile",
+  "email",
+  "read:todoitem",
+  "read:user",
+  "edit:todoitem",
+  "edit:user",
+  "delete:todoitem",
+  "delete:user",
+  "write:user",
+  "write:todoitem",
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/verify-user`,
+        audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        scope: requestedScopes.join(" "),
+      }}
+    >
+      <AuthTokenProvider>
+        <App />
+      </AuthTokenProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
