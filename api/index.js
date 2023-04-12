8f7f09dec5331ff19ddf0d7a23b0ac28aef8e796
Max <mikowind@outlook.com>
2023-04-12 16:38:51 -0700

    add requireAuth in api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import pkg from "@prisma/client";
 import morgan from "morgan";
 import cors from "cors";
+import { auth } from "express-oauth2-jwt-bearer";
+
+const requireAuth = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER,
+  tokenSigningAlg: "RS256",
+});
 
 const app = express();
 
