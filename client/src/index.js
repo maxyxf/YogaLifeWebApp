d86364c89d183a00daae303b03a49a13c6224758
Max <mikowind@outlook.com>
2023-04-12 14:12:02 -0700

    delete extra files

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import ProductDetail from "./screens/ProductDetail";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import { AuthTokenProvider } from "./AuthTokenContext";
@@ -61,8 +60,3 @@ root.render(
     </Auth0Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
