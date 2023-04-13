2d245138f6d25a1617944573db102a7d16cf17be
Max <mikowind@outlook.com>
2023-04-13 13:46:25 -0700

    show user's cart items in cartScreen

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,5 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useAuthToken } from "../AuthTokenContext";
 
 export default function CartScreen() {
-  return <p>CartScreen</p>;
+  const [cartItems, setCartItems] = useState([]);
+  const { accessToken } = useAuthToken();
+
+  useEffect(() => {
+    async function getCartItemsFromApi() {
+      const data = await fetch(`${process.env.REACT_APP_API_URL}/cart`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const cart = await data.json();
+
+      setCartItems(cart.products);
+    }
+
+    if (accessToken) {
+      getCartItemsFromApi();
+    }
+  }, [accessToken]);
+
+  console.log(cartItems);
+  return (
+    <div>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <img src={item.imageSrc} alt={item.name} width="100" height="100" />
+          <p>{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
