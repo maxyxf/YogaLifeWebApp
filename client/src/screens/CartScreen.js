b98cea572f8e248992f8a87afd29883c3541ce36
Max <mikowind@outlook.com>
2023-04-13 15:06:04 -0700

    create function to remove a product in user's cart

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,10 +1,29 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { useAuthToken } from "../AuthTokenContext";
 import useCartItems from "../hooks/useCartItems";
 
 export default function CartScreen() {
-  const [cartItems] = useCartItems();
+  const [cartItems, setCartItems] = useCartItems();
+  const { accessToken } = useAuthToken();
+
+  async function removeProductFromCart(productId) {
+    const data = await fetch(`${process.env.REACT_APP_API_URL}/cart/product`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        productId: productId,
+      }),
+    });
+    if (data.ok) {
+      const updatedCart = await data.json();
+      setCartItems(updatedCart.products);
+    } else {
+      return null;
+    }
+  }
 
   return (
     <div>
@@ -13,6 +32,7 @@ export default function CartScreen() {
           <p>{item.name}</p>
           <img src={item.imageSrc} alt={item.name} width="100" height="100" />
           <p>{item.price}</p>
+          <button onClick={() => removeProductFromCart(item.id)}>Delete</button>
         </div>
       ))}
     </div>
