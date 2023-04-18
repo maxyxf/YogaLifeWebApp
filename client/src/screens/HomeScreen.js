17d4aa8e5aabad02013e416eed66629df4e2d60d
Max <mikowind@outlook.com>
2023-04-18 03:14:16 -0700

    updates home screen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductList from "../components/ProductList";
+import { Link } from "react-router-dom";
 
 export default function HomeScreen() {
   return (
@@ -9,7 +10,7 @@ export default function HomeScreen() {
           <div className="relative overflow-hidden rounded-lg">
             <div className="absolute inset-0">
               <img
-                src="https://tailwindui.com/img/ecommerce-images/home-page-01-feature-section-01.jpg"
+                src="https://www.rei.com/dam/content_team_053017_81385_yoga_gear_how_choose_lg.jpg?t=ea16by9lg"
                 alt=""
                 className="h-full w-full object-cover object-center"
               />
@@ -17,21 +18,20 @@ export default function HomeScreen() {
             <div className="relative bg-gray-900 bg-opacity-75 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
               <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
                 <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                  <span className="block sm:inline">Level up</span>
-                  <span className="block sm:inline">your desk</span>
+                  <span className="block sm:inline">Yoga Life</span>
                 </h2>
                 <p className="mt-3 text-xl text-white">
-                  Make your desk beautiful and organized. Post a picture to
-                  social media and watch it get more likes than life-changing
-                  announcements. Reflect on the shallow nature of existence. At
-                  least you have a really nice desk setup.
+                  Welcome to Yogalife, your go-to source for premium yoga mats
+                  and towels. We offer a wide range of high-quality products
+                  that are designed to enhance your practice. Browse our
+                  selection today and discover the difference that a great mat
+                  or towel can make!
                 </p>
-                <a
-                  href="#"
-                  className="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-                >
-                  Shop Workspace
-                </a>
+                <Link to="/products">
+                  <button className="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">
+                    Shop Now
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
