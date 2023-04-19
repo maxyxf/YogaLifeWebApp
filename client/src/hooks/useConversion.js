9e89d0dde00170120b6e832a359b1cea34401b1d
Max <mikowind@outlook.com>
2023-04-19 16:29:55 -0700

    fix conversionRate temporarily

--- client/src/hooks/useConversion.js
+++ client/src/hooks/useConversion.js
@@ -1,23 +1,23 @@
 import { useState, useEffect } from "react";
 
 export default function useConversion() {
-  const [conversionRate, setConversionRate] = useState(1); // default currency is CAD
+  const [conversionRate, setConversionRate] = useState(0.74); // default currency is CAD
 
-  useEffect(() => {
-    const fetchConversionRate = async () => {
-      try {
-        const response = await fetch(
-          `https://v6.exchangerate-api.com/v6/bfb9820b5f4ef3d5d5cad1c7/latest/CAD`
-        );
-        const data = await response.json();
-        const rate = data.conversion_rates.USD;
-        setConversionRate(rate);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchConversionRate();
-  }, []);
+  // useEffect(() => {
+  //   const fetchConversionRate = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `https://v6.exchangerate-api.com/v6/716cb25a0fe1687ef9918db1/latest/CAD`
+  //       );
+  //       const data = await response.json();
+  //       const rate = data.conversion_rates.USD;
+  //       setConversionRate(rate);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchConversionRate();
+  // }, []);
 
   return [conversionRate, setConversionRate];
 }
