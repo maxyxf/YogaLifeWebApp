31bae14716371a0de3c4f664425bf1b50de1fdb9
Max <mikowind@outlook.com>
2023-04-22 00:12:01 -0700

    remove QuestionMarkCircleIcon

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -265,18 +265,6 @@ export default function CartScreen() {
               <div className="flex items-center justify-between border-t border-gray-200 pt-4">
                 <dt className="flex items-center text-sm text-gray-600">
                   <span>Shipping estimate</span>
-                  <a
-                    href="#"
-                    className="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500"
-                  >
-                    <span className="sr-only">
-                      Learn more about how shipping is calculated
-                    </span>
-                    <QuestionMarkCircleIcon
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  </a>
                 </dt>
                 <dd className="text-sm font-medium text-gray-900">
                   {currency === "CAD"
@@ -288,18 +276,6 @@ export default function CartScreen() {
               <div className="flex items-center justify-between border-t border-gray-200 pt-4">
                 <dt className="flex text-sm text-gray-600">
                   <span>Tax estimate</span>
-                  <a
-                    href="#"
-                    className="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500"
-                  >
-                    <span className="sr-only">
-                      Learn more about how tax is calculated
-                    </span>
-                    <QuestionMarkCircleIcon
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  </a>
                 </dt>
                 <dd className="text-sm font-medium text-gray-900">
                   {" "}
